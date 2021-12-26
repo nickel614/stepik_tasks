b7772979ef1b31efbcf79f47d1b01f41bf1a344f
Nikita Kazakov <nickel614@mail.ru>
2021-12-26 18:37:53 +0500

    Update rubb_matrix.py

--- rubb_matrix.py
+++ rubb_matrix.py
@@ -8,117 +8,172 @@ class Matrix:
         self.size = len(self.matrix)
 
     def __str__(self):
-        s = ''
-        for i in self.matrix:
-            for j in i: s += f'{j} '
-            s = s[:len(s) - 1]
-            s += '\n'
+        s = '\n'.join([''.join([f'{n} ' for n in row]) for row in self.matrix])
         return s
 
-    def pop(self): #метод возвращает удаленный элемент из матрицы, находящийся в ее конце
+    @staticmethod
+    def _full_mass_iterator(size):
+        for i in range(size):
+            for j in range(size):
+                yield i, j
+
+    @staticmethod
+    def _matrix_shift(m):
+        size = len(m)
+        for i in range(len(m) - 1)[:-1]:
+            nones = m[i].count(None)
+            for j in range(nones):
+                m[i].remove(None)
+            for j in range(nones):
+                m[i].append(m[i + 1][j])
+            for j in range(nones):
+                m[i + 1].pop(0)
+                m[i + 1].append(None)
+        return m
+
+    @staticmethod
+    def _matrix_expanding(m):
+        size = len(m)
+        m.append([])
+        for i in range(size):
+            m[len(m) - 1].append(None)
+        for i in range(len(m)):
+            m[i].append(None)
+        return m
+
+    @staticmethod
+    def _put_in_matrix(m, element):
+        i1 = j1 = len(m)**2 - 1
+        iter = list(Matrix._full_mass_iterator(len(m)))
+        for i, j in iter[::-1]:
+            if m[i][j] is not None and m[i1][j1] is None:
+                m[i1][j1] = element
+                return m
+            i1 = i
+            j1 = j
+
+    @staticmethod
+    def _matrix_reduction(self):
+        m1 = []
+        for i, j in self._full_mass_iterator(self.size):
+            if self.matrix[i][j] is None:
+                self.size -= 1
+                break
+            m1.append(self.matrix[i][j])
+        self.matrix = [[None for j in range(self.size)] for i in range(self.size)]
+        for i, j in self._full_mass_iterator(self.size):
+            try:
+                self.matrix[i][j] = m1.pop(0)
+            except:
+                return self.matrix
+
+    def pop(self) -> object:
+        """
+        метод возвращает удаленный элемент из матрицы, находящийся в ее конце
+        :return:
+        """
         if self.size == 1:
             raise IndexError
         m = self.matrix
-        def full_mass_iterator(size):
-            for i in range(size):
-                for j in range(size): yield i, j
-
-        def matrix_deexpanding(self):
-            m1 = []
-            for i, j in full_mass_iterator(self.size):
-                if self.matrix[i][j] == None:
-                    self.size -= 1
-                    break
-                m1.append(self.matrix[i][j])
-            self.matrix = [[None for j in range(self.size)] for i in range(self.size)]
-            for i, j in full_mass_iterator(self.size):
-                try:
-                    self.matrix[i][j] = m1.pop(0)
-                except:
-                    return self.matrix
 
         prev, i1, j1 = None, 0, 0
-        for i, j in full_mass_iterator(self.size):
-            if prev != None and m[i][j] == None:
+        for i, j in self._full_mass_iterator(self.size):
+            if prev is not None and m[i][j] is None:
                 popped = prev
                 m[i1][j1] = None
-                if i * self.size + j == (self.size - 1) ** 2 - (self.size - 1) + 1:
-                    m = matrix_deexpanding(self)
+                if i*self.size + j == (self.size-1)**2 - (self.size-1) + 1:
+                    m = self._matrix_reduction()
                 return popped
             prev = m[i][j]
             i1 = i
             j1 = j
 
-    def append(self, element=None):
-        def matrix_expanding(m):
-            size = len(m)
-            m.append([])
-            for i in range(size): m[len(m) - 1].append(None)
-            for i in range(len(m)): m[i].append(None)
-            return m
-
-        def matrix_shift(m):
-            size = len(m)
-            for i in range(len(m) - 1)[:size - 1]:
-                nones = m[i].count(None)
-                for j in range(nones): m[i].remove(None)
-                for j in range(nones): m[i].append(m[i + 1][j])
-                for j in range(nones):
-                    m[i + 1].pop(0)
-                    m[i + 1].append(None)
-            return m
-
-        if element == None: return self.matrix
-
+    def append(self, element=None) -> list:
+        """
+         Добавляет элемент element в матрицу и возвращает ее. Если последний элемент попадает в последнюю строку
+         матрицы, расширяет матрицу.
+        :param element:
+        :return:
+        """
+        if element is None:
+            return self.matrix
         if len(self.matrix) == 1:
             self.matrix[0][0] = element
-            self.matrix = matrix_expanding(self.matrix)
-            self.matrix = matrix_shift(self.matrix)
+            self.matrix = self._matrix_expanding(self.matrix)
+            self.matrix = self._matrix_shift(self.matrix)
             self.size += 1
             return self.matrix
-
         if len(self.matrix) < self.max_size:
-            size = len(self.matrix)
-            if self.matrix[-2][-1] != None:
-                self.matrix = matrix_expanding(self.matrix)
-                self.matrix = matrix_shift(self.matrix)
+            if self.matrix[-2][-1] is not None:
+                self._matrix_expanding(self.matrix)
+                self._matrix_shift(self.matrix)
                 self.size += 1
-                size += 1
-            for i in range(size):
-                for j in range(size):
-                    if self.matrix[i][j] == None:
-                        self.matrix[i][j] = element
-                        return self.matrix
+            self._put_in_matrix(self.matrix, element)
+            return self.matrix
         else:
-            if self.matrix[self.max_size - 1][self.max_size - 1] != None: raise IndexError
-            for i1, i in enumerate(iterable=self.matrix):
-                for j1, j in enumerate(iterable=i):
-                    if self.matrix[i1][j1] == None:
-                        self.matrix[i1][j1] = element
-                        return self.matrix
+            if self.matrix[self.max_size - 1][self.max_size - 1] is not None:
+                raise IndexError
+            self._put_in_matrix(self.matrix, element)
+            return self.matrix
         return self.matrix
 
     @classmethod
-    def from_iter(cls, iter_obj, max_size=None):
-        try: iter(iter_obj)
-        except: raise TypeError
+    def from_iter(cls, iter_obj, max_size=None) -> object:
+        """
+        Создает матрицу размером max_size, заполняет ее из iter_obj и возвращает заполненную матрицу
+        :param iter_obj:
+        :param max_size:
+        :return:
+        """
+        from math import ceil, sqrt
+        try:
+            iter(iter_obj)
+        except:
+            raise TypeError
 
-        m = Matrix(max_size)
+        checked_iter_object = []
         for i in iter_obj:
-            m.append(i)
+            if i is None:
+                continue
+            checked_iter_object.append(i)
+        if checked_iter_object:
+            iter_obj = checked_iter_object
+        else:
+            mtrx = Matrix()
+            return mtrx
+
+        mtrx = Matrix(max_size)
+        size = ceil(sqrt(len(iter_obj)))
+        if len(iter_obj) <= size**2 - size:
+            mtrx.matrix = [[None for i in range(size)] for j in range(size)]
+            for i, j in mtrx._full_mass_iterator(size):
+                try:
+                    mtrx.matrix[i][j] = iter_obj[0]
+                    del iter_obj[0]
+                except IndexError:
+                    break
+        else:
+            mtrx.matrix = [[None for i in range(size+1)] for j in range(size+1)]
+            for i, j in mtrx._full_mass_iterator(size + 1):
+                try:
+                    mtrx.matrix[i][j] = iter_obj[0]
+                    del iter_obj[0]
+                except IndexError:
+                    mtrx._matrix_shift(mtrx.matrix)
+                    break
+        return mtrx
+
+    @classmethod
+    def _from_iter(cls, iter_obj, max_size=None) -> object:
+        m = Matrix(max_size)
+        for e in iter_obj:
+            m.append(e)
         return m
 
 
+t1 = time.time()
+mtrx = Matrix._from_iter(range(10), max_size=3)
+t2 = time.time()
 
-for i in range(5):
-    t1 = time.time()
-    mtrx = Matrix.from_iter([1,2,3,4,5,6,7,8], 3)
-    t2 = time.time()
-    mtrx.append(1)
-    t3 = time.time()
-    for i in range(7):
-        mtrx.pop()
-    t4 = time.time()
-    print(f'creation: {t2 - t1}')
-    print(f'appending: {t3 - t2}')
-    print(f'popping: {t4 - t3}')
+print(f'Creation: {t2 - t1}')
+print(mtrx)
